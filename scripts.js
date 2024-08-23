be93291de0c445dd15e241c7362c5f042f6eb142
nyaliteCA <nyalite@gmail.com>
2024-08-22 23:00:14 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,187 +1,134 @@
- /*global $ */
- 
 var goal = 226;
 var gear = 0;
 var flask = false;
 var juju = false;
-var cloak = false
-var helm = false
-var pants = false
-var shoulder = false
-var shoulder2 = false
+var cloak = false;
+var helm = false;
+var pants = false;
+var shoulder = false;
+var shoulder2 = false;
 
 $(document).ready(function(){
-	$("#frInputField").val(0);
-  $("#frInputField").change(function(){
-  if ($("#frInputField").val() <= 315 && $("#frInputField").val() >= 0){
-	  gear = parseInt($("#frInputField").val());
-  } else {
-	  console.log("invalid number");
-  }
-  computeFr();
-  });
+    $("#frInputField").val(gear);
+    $("#frInputField").change(function(){
+        if ($(this).val() <= 315 && $(this).val() >= 0){
+            gear = parseInt($(this).val());
+        } else {
+            console.log("invalid number");
+            $(this).val(gear);
+        }
+        computeFr();
+    });
+
+    $(".item").click(function() {
+        var id = $(this).attr('id');
+        switch(id) {
+            case 'frFlask':
+                flask = !flask;
+                break;
+            case 'frJuju':
+                juju = !juju;
+                break;
+            case 'frHelm':
+                helm = !helm;
+                break;
+            case 'frCloak':
+                cloak = !cloak;
+                break;
+            case 'frPants':
+                pants = !pants;
+                break;
+            case 'frShoulder':
+                shoulder = !shoulder;
+                if (shoulder) {
+                    shoulder2 = false;
+                    updateButtonStatus('frShoulder2');
+                }
+                break;
+            case 'frShoulder2':
+                shoulder2 = !shoulder2;
+                if (shoulder2) {
+                    shoulder = false;
+                    updateButtonStatus('frShoulder');
+                }
+                break;
+        }
+        updateButtonStatus(id);
+        computeFr();
+    });
+
+    computeFr();
 });
 
- //Buttons
- $("#frFlaskBtn").on("click", function() {
-	 if (flask == false) {
-		$("#frFlaskBtn").removeClass("btn-secondary").addClass("btn-success");
-		$("#flaskBtnIcon").attr("src", "img/check2.svg");
-		flask = true;
-	 } else {
-		 $("#frFlaskBtn").removeClass("btn-success").addClass("btn-secondary");
-		 $("#flaskBtnIcon").attr("src", "img/x-lg.svg");
-		 flask = false;
-	 }
-	  computeFr();
- });
-  $("#frJujuBtn").on("click", function() {
-	 if (juju == false) {
-		$("#frJujuBtn").removeClass("btn-secondary").addClass("btn-success");
-		$("#jujuBtnIcon").attr("src", "img/check2.svg");
-		juju = true;
-	 } else {
-		 $("#frJujuBtn").removeClass("btn-success").addClass("btn-secondary");
-		 $("#jujuBtnIcon").attr("src", "img/x-lg.svg");
-		 juju = false;
-	 }
-	  computeFr();
- });
-  $("#frHelmBtn").on("click", function() {
-	 if (helm == false) {
-		$("#frHelmBtn").removeClass("btn-secondary").addClass("btn-success");
-		$("#HelmBtnIcon").attr("src", "img/check2.svg");
-		helm = true;
-	 } else {
-		 $("#frHelmBtn").removeClass("btn-success").addClass("btn-secondary");
-		 $("#HelmBtnIcon").attr("src", "img/x-lg.svg");
-		 helm = false;
-	 }
-	  computeFr();
- });
-  $("#frCloakBtn").on("click", function() {
-	 if (cloak == false) {
-		$("#frCloakBtn").removeClass("btn-secondary").addClass("btn-success");
-		$("#cloakBtnIcon").attr("src", "img/check2.svg");
-		cloak = true;
-	 } else {
-		 $("#frCloakBtn").removeClass("btn-success").addClass("btn-secondary");
-		 $("#cloakBtnIcon").attr("src", "img/x-lg.svg");
-		 cloak = false;
-	 }
-	  computeFr();
- });
-  $("#frPantsBtn").on("click", function() {
-	 if (pants == false) {
-		$("#frPantsBtn").removeClass("btn-secondary").addClass("btn-success");
-		$("#pantsBtnIcon").attr("src", "img/check2.svg");
-		pants = true;
-	 } else {
-		 $("#frPantsBtn").removeClass("btn-success").addClass("btn-secondary");
-		 $("#pantsBtnIcon").attr("src", "img/x-lg.svg");
-		 pants = false;
-	 }
-	  computeFr();
- });
-  $("#frShoulderBtn").on("click", function() {
-	 if (shoulder == false) {
-		$("#frShoulderBtn").removeClass("btn-secondary").addClass("btn-success");
-		$("#shoulderBtnIcon").attr("src", "img/check2.svg");
-		$("#frShoulder2Btn").removeClass("btn-success").addClass("btn-secondary");
-		$("#shoulder2BtnIcon").attr("src", "img/x-lg.svg");
-		shoulder = true;
-		shoulder2 = false;
-	 } else{
-		 $("#frShoulderBtn").removeClass("btn-success").addClass("btn-secondary");
-		$("#shoulderBtnIcon").attr("src", "img/x-lg.svg");
-		shoulder = false;
-	 }
-		 computeFr();
- });
- 
-   $("#frShoulder2Btn").on("click", function() {
-	 if (shoulder2 == false) {
-		$("#frShoulder2Btn").removeClass("btn-secondary").addClass("btn-success");
-		$("#shoulder2BtnIcon").attr("src", "img/check2.svg");
-		$("#frShoulderBtn").removeClass("btn-success").addClass("btn-secondary");
-		$("#shoulderBtnIcon").attr("src", "img/x-lg.svg");
-		shoulder2 = true;
-		shoulder = false;
-	 } else{
-		 $("#frShoulder2Btn").removeClass("btn-success").addClass("btn-secondary");
-		$("#shoulderBtn2Icon").attr("src", "img/x-lg.svg");
-		shoulder2 = false;
-	 }
-	  computeFr();
- });
- 
- 
- 
- /*
- <div class="card mx-auto text-white bg-primary mb-3">
-   <div class="card-header"><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample">Open</button>Step 1</div>
-   <div class="collapse card-body" id="collapseExample">
-     <h5 class="card-title">Accept Quest</h5>
-     <p class="card-text">some reqs</p>
-   </div>
- </div>
- */
- //Functions
+function updateButtonStatus(id) {
+    var isActive = false;
+    switch(id) {
+        case 'frFlask':
+            isActive = flask;
+            break;
+        case 'frJuju':
+            isActive = juju;
+            break;
+        case 'frHelm':
+            isActive = helm;
+            break;
+        case 'frCloak':
+            isActive = cloak;
+            break;
+        case 'frPants':
+            isActive = pants;
+            break;
+        case 'frShoulder':
+            isActive = shoulder;
+            break;
+        case 'frShoulder2':
+            isActive = shoulder2;
+            break;
+    }
+    
+    if (isActive) {
+        $(`#${id}`).addClass('btn-success').removeClass('btn-secondary');
+        $(`#${id} .status-icon`).html('&#10004;'); // Unicode check mark
+    } else {
+        $(`#${id}`).removeClass('btn-success').addClass('btn-secondary');
+        $(`#${id} .status-icon`).html(''); // Empty when deselected
+    }
+}
+
 function computeFr(){
-	var fr = gear;
-	if (flask){
-		fr += 25;
-	}
-	if (juju) {
-		fr += 15;
-	}
-	if (helm)
-	{
-		fr += 20;
-	}
-	if (pants)
-	{
-		fr += 20;
-	}
-	if (cloak)
-	{
-		fr += 20;
-	}
-	if (shoulder)
-	{
-		fr += 15;
-	}
-	if (shoulder2)
-	{
-		fr += 5;
-	}
-	$("#frTotal").text(fr);
-	//check mark
-	var markFr = fr +20;
-	var impMarkFr = fr + 27;
-	var totemFr = fr +60;
-	
-	if (markFr >= 226 ) {
-		$("#resultMark").text("Safe (+" + (markFr-goal) + ")");
-	} else if (markFr >= 196) {
-		$("#resultMark").text("Burnt (" + (markFr-goal) + ")");
-	} else {
-		$("#resultMark").text("Cooked (" + (markFr-goal) + ")");
-	}
-	//check imp mark
-	if (impMarkFr >= 226 ) {
-		$("#resultImpMark").text("Safe (+" + (impMarkFr-goal) + ")");
-	} else if (impMarkFr >= 196) {
-		$("#resultImpMark").text("Burnt (" + (impMarkFr-goal) + ")");
-	} else {
-		$("#resultImpMark").text("Cooked (" + (impMarkFr-goal) + ")");
-	}
-	//check totem
-	if (totemFr >= 226 ) {
-		$("#resultTotem").text("Safe (+" + (totemFr-goal) + ")");
-	} else if (totemFr >= 196) {
-		$("#resultTotem").text("Burnt (" + (totemFr-goal) + ")");
-	} else {
-		$("#resultTotem").text("Cooked (" + (totemFr-goal) + ")");
-	}
-};
+    var fr = gear;
+    if (flask) fr += 25;
+    if (juju) fr += 15;
+    if (helm) fr += 20;
+    if (pants) fr += 20;
+    if (cloak) fr += 20;
+    if (shoulder) fr += 15;
+    if (shoulder2) fr += 5;
+    
+    $("#frTotal").text(fr);
+    
+    var markFr = fr + 20;
+    var impMarkFr = fr + 27;
+    var totemFr = fr + 60;
+    
+    updateBuffResult(markFr, "#resultMark");
+    updateBuffResult(impMarkFr, "#resultImpMark");
+    updateBuffResult(totemFr, "#resultTotem");
+}
+
+function updateBuffResult(fr, elementId) {
+    var result, className;
+    if (fr >= goal) {
+        result = `Safe (+${fr - goal})`;
+        className = 'safe';
+    } else if (fr >= goal-30) {
+        result = `Burnt (${fr - goal})`;
+        className = 'burnt';
+    } else {
+        result = `Cooked (${fr - goal})`;
+        className = 'cooked';
+    }
+    
+    $(elementId).removeClass('safe burnt cooked').addClass(className);
+	$(elementId).find('.item-effect').removeClass('safe burnt cooked').addClass(className).text(result);
+}
